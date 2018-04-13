92c9374098ebdaa555d85c378977281988e107d7
Larry Botha <larry@fixate.it>
2018-04-13 09:57:45 +0200

    demonstrate stream similarity to array

--- 01/index.js
+++ 01/index.js
@@ -3,3 +3,11 @@ const Rx = require('rxjs');
 const plainArray = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'];
 
 console.log('plainArray', plainArray);
+// With an Observable we don't know when the events will be accessible
+const stream = Rx.Observable.interval(400)
+  .take(9)
+  .map(i => plainArray[i]);
+
+// we subscribe to an observable in order to consume it
+// 'subscribe' is called an event listener. stream is the source of the events
+stream.subscribe(x => console.log('stream: ', x));
