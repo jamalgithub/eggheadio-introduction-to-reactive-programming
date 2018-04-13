73b931e2a8d1badae31c94249729e4fc9293bd0a
Larry Botha <larry@fixate.it>
2018-04-13 10:44:44 +0200

    add intro notes

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,9 @@
 const {Observable} = Rx;
+
+const label = document.querySelector('label');
+const button = document.querySelector('button');
+
+// To manually handle double clicks we would need:
+// a counter
+// a timer
+// With reactive streams we don't need any of that
